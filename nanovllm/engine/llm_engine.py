96350f89d12c152a40b8ab07313b9f13d3c0b78e
ld <2559978119@qq.com>
2025-11-16 18:09:24 +0800

    time fix

--- nanovllm/engine/llm_engine.py
+++ nanovllm/engine/llm_engine.py
@@ -87,24 +87,25 @@ class LLMEngine:
                     
         outputs = {}
         prefill_throughput = decode_throughput = 0.
+        start_time = perf_counter()
         while not self.is_finished():
             t = perf_counter()
             output, num_tokens = self.step()
-            step_time = perf_counter() - t
             if use_tqdm:
                 if num_tokens > 0:
-                    prefill_throughput = num_tokens / (step_time)
+                    prefill_throughput = num_tokens / (perf_counter() - t)
                 else:
                     decode_throughput = -num_tokens / (perf_counter() - t)
                 pbar.set_postfix({
                     "Prefill": f"{int(prefill_throughput)}tok/s",
-                    "PrefillTime": f"{step_time * 1000:.3f} ms",
                     "Decode": f"{int(decode_throughput)}tok/s",
                 })
             for seq_id, token_ids in output:
                 outputs[seq_id] = token_ids
                 if use_tqdm:
                     pbar.update(1)
+        end_time = perf_counter()
+        print(f"总时间: {(end_time - start_time)*1000:.2f}ms")
         outputs = [outputs[seq_id] for seq_id in sorted(outputs.keys())]
         outputs = [{"text": self.tokenizer.decode(token_ids), "token_ids": token_ids} for token_ids in outputs]
         if use_tqdm:
